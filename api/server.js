1d0838a747d0425d959079d9bd6d47dd48b5b3d1
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-12-16 08:46:49 -0800

    basic server setup with express

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.json('Hello World');
+});
+
+module.exports = server;
